b1258169120127c0ed4a55f1aebb0522ae6dcef0
Michael Snowden <michael.snowden@glassdoor.com>
2015-12-24 03:19:25 -0800

    Added permissions to compilation script in installation script

--- poe.sh
+++ poe.sh
@@ -6,5 +6,6 @@ cd ~/poe-install
 git clone https://github.com/MichaelSnowden/poe .
 mvn generate-sources
 mvn clean compile assembly:single
-echo "java -jar $(pwd)/target/poe.jar $1" >> poec.sh
+echo "java -jar $(pwd)/target/poe.jar \$1" >> poec.sh
+sudo chmod 777 poec.sh
 sudo cp poec.sh /usr/bin/poec
