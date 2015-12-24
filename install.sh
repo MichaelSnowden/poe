979bafcf942635e1f2e2e7df1ebc15752fcf3152
Michael Snowden <michael.snowden@glassdoor.com>
2015-12-24 03:31:54 -0800

    Added a descriptive post-installation message

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ echo "#!/usr/bin/env bash" >> poe.sh
 echo "java -jar $(pwd)/target/poe.jar \$1" >> poe.sh
 sudo chmod 777 poe.sh
 sudo cp poe.sh /usr/bin/poe
+echo "Installation successful!"
+echo "You can now run POE files using the 'poe' command"
+echo "Try this as an example"
+echo "  echo -n 'Say hi.' >> test.poe"
+echo "  poe test.poe"
