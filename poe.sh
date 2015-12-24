87d2f3c1aa6d0bbb2959da3dc1e7a2464d7c10d4
Michael Snowden <michael.snowden@glassdoor.com>
2015-12-24 03:06:31 -0800

    Added a compilation shell script

--- poe.sh
+++ poe.sh
@@ -6,10 +6,4 @@ cd ~/poe-install
 git clone https://github.com/MichaelSnowden/poe .
 mvn generate-sources
 mvn clean compile assembly:single
-echo "export CLASSPATH=\"${CLASSPATH}:$(pwd)/target/poe.jar\"" >> ~/.bashrc
-echo "export PATH=\"\$PATH:$(pwd)/target/poe.jar\"" >> ~/.bashrc
-echo "chown -R $(whoami) ~/poe-install" >> ~/.bashrc
-source ~/.bashrc
-echo "Say hi." >> test.poe
-java -jar poe.jar test.poe
-rm test.poe
+sudo cp poec.sh /usr/bin/poec
