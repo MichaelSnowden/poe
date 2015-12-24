36a8640ebfcdca4b6a76237577c9a05309ac0a0c
Michael Snowden <michael.snowden@glassdoor.com>
2015-12-24 03:21:40 -0800

    Attempting to remove compile script from VCS

--- poe-install.sh
+++ poe-install.sh
@@ -6,6 +6,7 @@ cd ~/poe-install
 git clone https://github.com/MichaelSnowden/poe .
 mvn generate-sources
 mvn clean compile assembly:single
-echo "java -jar $(pwd)/target/poe.jar \$1" >> poec.sh
-sudo chmod 777 poec.sh
-sudo cp poec.sh /usr/bin/poec
+echo "#!/usr/bin/env bash" >> poe.sh
+echo "java -jar $(pwd)/target/poe.jar \$1" >> poe.sh
+sudo chmod 777 poe.sh
+sudo cp poe.sh /usr/bin/poec
